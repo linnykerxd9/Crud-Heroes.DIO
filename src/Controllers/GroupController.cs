561802730d59f27c57873e624b3cbbe05651b03e
linnykerxd9 <linnykerxd9@gmail.com>
2021-11-04 15:02:34 -0300

    Group controller and heroes controller finished

---
+++ src/Controllers/GroupController.cs
@@ -0,0 +1,59 @@
+﻿using CRUD.EfCore.src.Database;
+using CRUD.EfCore.src.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.EfCore.src.Controllers
+{
+    [ApiController]
+    [Route("api/v1/group")]
+    public class GroupController 
+    {
+        private DataContext _context;
+
+        public GroupController(DataContext context)
+        {
+            this._context = context;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Group>> Create(Group group)
+        {
+
+            _context.Groups.Add(group);
+            await _context.SaveChangesAsync();
+
+            return group;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Group>> GetById(int id)
+        {
+            Group group = await _context.Groups.FirstOrDefaultAsync(h => h.Id == id);
+            return group;
+        }
+
+
+        [HttpPut()]
+        public async Task<ActionResult<Group>> UpdateHero(Group group)
+        {
+            _context.Groups.Update(group);
+            await _context.SaveChangesAsync();
+
+            return group;
+        }
+
+        [HttpDelete()]
+        public async Task<ActionResult<Group>> DeleteHero(Group group)
+        {
+            _context.Groups.Remove(group);
+            await _context.SaveChangesAsync();
+
+            return group;
+        }
+    }
+}
--- src/Controllers/HeroController.cs
+++ src/Controllers/HeroController.cs
@@ -2,7 +2,10 @@ using CRUD.EfCore.src.Database;
 using CRUD.EfCore.src.Dto;
 using CRUD.EfCore.src.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CRUD.EfCore.src.Controllers
 {
@@ -18,23 +21,42 @@ namespace CRUD.EfCore.src.Controllers
             this._context = context;
         }
 
+
         [HttpPost]
-        public HeroDTO Create(HeroDTO hero)
+        public async Task<ActionResult<Hero>> Create( HeroDTO hero)
         {
             Hero newHero = new Hero(hero);
 
             _context.Heroes.Add(newHero);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
-            return hero;
+            return newHero;
         }
 
         [HttpGet("{id:int}")]
-        public Hero GetById(int id)
+        public async Task<ActionResult<Hero>> GetById( int id)
         {
-            Hero hero = _context.Heroes.FirstOrDefault(h => h.Id == id);
+            Hero hero = await _context.Heroes.FirstOrDefaultAsync(h => h.Id == id);
             return hero;
         }
-        
+
+       
+        [HttpPut()]
+        public async Task<ActionResult<Hero>> UpdateHero( Hero hero)
+        {
+            _context.Heroes.Update(hero);
+            await _context.SaveChangesAsync();
+
+            return hero;
+        }
+
+        [HttpDelete()]
+        public async Task<ActionResult<Hero>> DeleteHero( Hero hero)
+        {
+            _context.Heroes.Remove(hero);
+            await _context.SaveChangesAsync();
+
+            return hero;
+        }
     }
 }
